45dadd17d426cacd2edadecb2e128754b52b90b1
Fun1234 <gth3283@gmail.com>
2024-10-13 02:15:49 +0900

    Update Player.cpp

--- Project/main project/Player.cpp
+++ Project/main project/Player.cpp
@@ -1,14 +1,23 @@
 #include "Player.h"
 
+Player::Player(int a, int b, int h, int av, int d, int he, int spd) {
+	player = State(a, b, h, av, d, he, spd);
+}
+
 void Player::act1() {
 	int pl;
+	cout << "Player의 공격! 행동을 선택하세요:" << endl;
+	cout << "1. 강공격" << endl;
+	cout << "2. 약공격" << endl;
+	cout << "선택: ";
 	cin >> pl;
 	switch (pl) {
 	case 1:
-		player.Attack1();
+
+		StateChange(1);
 		break;
 	case 2:
-		player.Attack2();
+		StateChange(2);
 		break;
 	default:
 		break;
@@ -17,16 +26,21 @@ void Player::act1() {
 
 void Player::act2() {
 	int pl;
+	cout << "Player의 수비! 행동을 선택하세요:" << endl;
+	cout << "1. 방어" << endl;
+	cout << "2. 회피" << endl;
+	cout << "3. 회복" << endl;
+	cout << "선택: ";
 	cin >> pl;
 	switch (pl) {
 	case 1:
-		player.Defense();
+		StateChange(3);
 		break;
 	case 2:
-		player.Avoid();
+		StateChange(4);
 		break;
 	case 3:
-		player.Heal();
+		StateChange(5);
 		break;
 	default:
 		break;
