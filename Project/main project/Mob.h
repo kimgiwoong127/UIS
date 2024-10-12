7686d4bf3c1dda31995ead5813f102b003e8656c
Fun1234 <gth3283@gmail.com>
2024-10-13 02:15:51 +0900

    Update Mob.h

--- Project/main project/Mob.h
+++ Project/main project/Mob.h
@@ -1,13 +1,13 @@
 #ifndef MOB_H
 #define MOB_H
 
-#include "State.h"
+#include "Player.h"
 #include <cstdlib> 
 #include <ctime>   
 
 using namespace std;
 
-class Mob :public State {
+class Mob {
 private:
 	int attack1W[3] = { 1,1,1 };
 	int attack1L[3] = { 1,1,1 };
@@ -20,11 +20,21 @@ private:
 	int AvoL[2] = { 1,1 };
 	int HealW[2] = { 1,1 };
 	int HealL[2] = { 1,1 };
-public:
 	State mob;
+public:
 	Mob() {};
-	Mob(int a, int b, int h, int av, int d, int he);
-	void turn1(State player);
-	void turn2(State player);
+	Mob(int a, int b, int h, int av, int d, int he,int spd);
+	int gHP() { return mob.hp; };
+	int max() { return mob.maxhp; }
+	void turn1(Player& p);
+	void turn2(Player& p);
+	void setSpeed(int spd) { mob.setSpeed(spd); }
+	int gSpeed() { return mob.gSpeed(); }
+	int gAttack1() const { return mob.gAttack1(); };
+	int gAttack2() const { return mob.gAttack2(); };
+	int gAvoid() const { return mob.gAvoid(); };
+	int gDefense() const { return mob.gDefense(); };
+	int gHeal() const { return mob.gHeal(); };
+	void sHP(int a) { mob.hp = a; };
 };
 #endif
