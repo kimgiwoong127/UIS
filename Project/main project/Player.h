58c5069ab9809ad41c72176a371789b138454cbe
Fun1234 <gth3283@gmail.com>
2024-10-13 02:15:48 +0900

    Update Player.h

--- Project/main project/Player.h
+++ Project/main project/Player.h
@@ -4,16 +4,24 @@
 #include "State.h"
 using namespace std;
 
-class Player :public State {
+class Player{
 private:
 	State player;
 public:
-	Player(int a, int b, int h, int av, int d, int he);
+	Player(int a, int b, int h, int av, int d, int he,int spd);
 	void act1();
 	void act2();
+	void setSpeed(int spd) { player.setSpeed(spd); }
+	int gHP() { return player.hp; }
+	int max() { return player.maxhp; }
+	int gSpeed() { return player.gSpeed(); }
+	void StateChange(int a) { player.setState(a); };
+	int PlayerState() const { return player.myState(); };
+	int gAttack1() const { return player.gAttack1(); };
+	int gAttack2() const { return player.gAttack2(); };
+	int gAvoid() const { return player.gAvoid(); };
+	int gDefense() const { return player.gDefense(); };
+	int gHeal() const { return player.gHeal(); };
+	void sHP(int a) { player.hp = a; };
 };
-
-Player::Player(int a, int b, int h, int av, int d, int he) {
-	player = State(a, b, h, av, d, he);
-}
 #endif
